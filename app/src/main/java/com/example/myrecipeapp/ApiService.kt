b7a6ffca86b222fa3fd214edce344c2b11f052dd
pr-Dutta <89duttapranjal@gmail.com>
2024-03-24 12:42:26 +0530

    new commit

--- app/src/main/java/com/example/myrecipeapp/ApiService.kt
+++ app/src/main/java/com/example/myrecipeapp/ApiService.kt
@@ -6,22 +6,38 @@ import retrofit2.http.GET
 
 // How can we setup a service?, Why we use service and what is it?
 
-// First we have to make a retrofit object
-// We have to build the retrofit url then We need to add a converterFactory to
-// convert JSON to kotlin object then we have to build it.
 
-// Create a retrofit instance
-private val retrofit = Retrofit.Builder().baseUrl("www.themealdb.com/api/json/v1/1/")
+// A retrofit instance is created specifying the base URl of the API and a
+// conversion factory to handel the conversion of JSON response to kotlin object
+
+// Retrofit.Builder() :- This initialize a new retrofit builder, builder is used to
+// configure and create a retrofit instance
+
+// .baseUrl() :- Here you set the base url for the API endpoints, All endpoint paths
+// specified in your API interface will be appended to this base url
+
+// .addConverterFactory(GsonConverterFactory.create()) : - This line adds a conversion
+// factory to the builder
+
+// .builder() :- Finally, this method call creates and returns a Retrofit instance
+private val retrofit = Retrofit.Builder()
+    .baseUrl("www.themealdb.com/api/json/v1/1/")
     .addConverterFactory(GsonConverterFactory.create())
     .build()
 
 // Create an API service instance
+
+// retrofit.create(ApiService::class.java) :- This line tells the Retrofit instance
+// to create an implementation of the ApiService interface
+
+// ApiService::class.java :- This path specifies the class type of the API service
+// interface that Retrofit should create an implementation for.
 val recipeService = retrofit.create(ApiService::class.java)
 
 
-// Define the API end points:- where the request will sent
+// An interface (ApiService) defines the API endpoint, and a request is made to fetch data
+// This is an API interface
 interface ApiService {
-    // Why we are defining the @GET inside the interface?
     @GET("categories.php")
     // This getCategories is a service method?
     suspend fun getCategories(): CategoriesResponse
