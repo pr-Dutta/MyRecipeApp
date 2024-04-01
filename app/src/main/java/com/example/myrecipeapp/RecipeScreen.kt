c48d3d711fb9ad266c423193cec2a44c9c63a2fa
pr-Dutta <89duttapranjal@gmail.com>
2024-04-01 20:51:40 +0530

    new learning

--- app/src/main/java/com/example/myrecipeapp/RecipeScreen.kt
+++ app/src/main/java/com/example/myrecipeapp/RecipeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.myrecipeapp
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
@@ -23,7 +24,10 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
 
 @Composable
-fun RecipeScreen(modifier: Modifier = Modifier) {
+fun RecipeScreen(
+    modifier: Modifier = Modifier,
+    navigateToDetail: (Category) -> Unit        // new
+) {
 
     // This is a ViewModel instance
     val recipeViewModel: MainViewModel = viewModel()
@@ -32,37 +36,44 @@ fun RecipeScreen(modifier: Modifier = Modifier) {
     Box(modifier = Modifier.fillMaxSize()) {
         when {
             viewState.loading -> {
-                CircularProgressIndicator(modifier.align(Alignment.Center))
+                CircularProgressIndicator(modifier.align(Alignment.Center))     // new
             }
             viewState.error != null -> {
                 Text(text = "ERROR OCCURRED")
             }
             else -> {
                 // Display categories
-                CategoryScreen(categories = viewState.list)
+                CategoryScreen(categories = viewState.list, navigateToDetail)       // new
             }
         }
     }
 }
 
 @Composable
-fun CategoryScreen(categories: List<Category>) {
+fun CategoryScreen(
+    categories: List<Category>,
+    navigateToDetail: (Category) -> Unit        // new
+) {
     // This should be learned
     LazyVerticalGrid(columns = GridCells.Fixed(2), modifier = Modifier.fillMaxSize()) {
         items(categories) {
             category ->
-            CategoryItem(category = category)
+            CategoryItem(category = category, navigateToDetail)         // new
         }
     }
 }
 
 // How each items looks like
 @Composable
-fun CategoryItem(category: Category) {
+fun CategoryItem(
+    category: Category,
+    navigateToDetail: (Category) -> Unit
+) {
     Column(
         modifier = Modifier
             .padding(8.dp)
-            .fillMaxSize(),
+            .fillMaxSize()
+            .clickable { navigateToDetail(category) },        // new
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
@@ -78,7 +89,7 @@ fun CategoryItem(category: Category) {
         Text(
             text = category.strCategory,
             color = Color.Black,
-            style = TextStyle(fontWeight = FontWeight.Bold),
+            style = TextStyle(fontWeight = FontWeight.Bold),        // new
             modifier = Modifier.padding(top = 4.dp)
         )
     }
